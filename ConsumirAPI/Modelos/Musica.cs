5d9c98d6a1feabf29861b38778507ba06c9f41af
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-08-08 19:51:46 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumirAPI/Modelos/Musica.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+namespace ConsumirAPI.Modelos;
+class Musica
+{
+    [JsonPropertyName("song")]
+    public string Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public int Genero { get; set; }
+
+
+    public void exibirDetalhes()
+    {
+        Console.WriteLine("Musica: " + Nome);
+        Console.WriteLine("Artista: " + Artista);
+        Console.WriteLine("Duração: " + Duracao / 1000);
+    }
+
+    public void exibirGenerosMusicais(List<Musica> musicas)
+    {
+        foreach(var musica in musicas)
+        {
+            Console.WriteLine(musica.Genero);
+        }
+    }
+
+
+
+
+}
---
+++ ConsumirAPI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using ConsumirAPI.Modelos;
+
+using (HttpClient client = new HttpClient())
+{
+
+    try
+    {
+        string reposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        //Console.WriteLine(reposta);
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(reposta);
+
+        
+
+    }catch (Exception ex)
+    {
+        Console.WriteLine("O programa não rodou pois: "+ ex.Message);
+    }
+
+
+
+
+    
+}
